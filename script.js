5f5e317b0e6423e5f45c9e7f1990b148fe6e4b55
Jeffrryy <118794827+Jeffrryy@users.noreply.github.com>
2023-02-28 11:42:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let phi;
+let jari2;
+let total;
+
+function jumlahluaslingkaran(a){
+    jari2= a**2
+phi=3.14;
+    total = phi * jari2;
+    return total;
+
+}
+
+alert(jumlahluaslingkaran(10));
